a1549b05cd80b290d7a47e0ac0309bb52090d314
nicolas-martin <nicolas.martin.0986@gmail.com>
2024-12-30 03:48:22 -0500

    init

--- MemeTraderMobile/src/navigation/types.ts
+++
@@ -1,9 +0,0 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
-export type RootStackParamList = {
-  Main: undefined;
-  CoinDetail: { coinId: string };
-};
-
-export type MainScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Main'>;
-export type CoinDetailScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'CoinDetail'>; 
